9aa9880774ef18d0ecc0886b1c6b65b33eb9ef26
Anar <rahanar@gmail.com>
2021-01-13 15:39:30 -0800

    Setup gorilla/mux, go.mod and update Dockerfile
    Setup a router using gorilla mux and added a simple handler. Setup
    go.mod to track dependencies. Updated Dockerfile to work in the new
    setup.

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := mux.NewRouter()
+	router.Handle("/", http.HandlerFunc(handle))
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello, World!")
 }
